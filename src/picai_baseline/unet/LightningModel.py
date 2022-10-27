136c339ca0759e8572cbb1adc877426d0c797aae
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-27 12:12:13 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/LightningModel.py
+++ src/picai_baseline/unet/LightningModel.py
@@ -128,12 +128,12 @@ class Model(pl.LightningModule):
     def validation_step(self, batch, batch_idx, dataloader_idx):
         valid_label, preds = self._shared_eval_step(batch, batch_idx)
         # revert horizontally flipped tta image
-        if(dataloader_idx==0):
-            return {'valid_label': valid_label, 'val_preds' : preds }
-        return {'train_label': valid_label, 'tain_preds' : preds }
+    return {'valid_label': valid_label, 'val_preds' : preds ,dataloader_idx :dataloader_idx}
 
-    def _eval_epoch_end(self, outputs,labelKey,predsKey):
+
+    def _eval_epoch_end(self, outputs,labelKey,predsKey, dataloader_idx):
         epoch=self.current_epoch
+        outpuuts = list(map( lambda entry : entry['dataloader_idx']==dataloader_idx,outputs))
         all_valid_labels=np.array(([x[labelKey].cpu().detach().numpy() for x in outputs]))
         all_valid_preds=np.array(([x[predsKey]for x in outputs]))
         # all_valid_labels=np.array(([x['valid_label'].cpu().detach().numpy() for x in outputs]))
@@ -159,8 +159,8 @@ class Model(pl.LightningModule):
     
 
     def validation_epoch_end(self, outputs): 
-        epoch,valid_metrics,num_pos,num_neg = self._eval_epoch_end( outputs,'valid_label','val_preds' )
-        epoch,valid_metrics_train,num_pos_train,num_neg_train = self._eval_epoch_end( outputs,'train_label','tain_preds' )
+        epoch,valid_metrics,num_pos,num_neg = self._eval_epoch_end( outputs,'valid_label','val_preds',0 )
+        epoch,valid_metrics_train,num_pos_train,num_neg_train = self._eval_epoch_end( outputs,'train_label','tain_preds' ,1)
 
         self.tracking_metrics['all_epochs'].append(epoch+1)
         # self.tracking_metrics['all_train_loss'].append(self.tracking_metrics['train_loss'])
