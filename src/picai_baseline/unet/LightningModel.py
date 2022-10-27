e1aea5152c2680a22a6c1d5ffbb428b4d9864ce0
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-27 11:42:15 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/LightningModel.py
+++ src/picai_baseline/unet/LightningModel.py
@@ -201,5 +201,6 @@ class Model(pl.LightningModule):
                 print("Validation Ranking Score Improved! Saving New Best Model", flush=True)
 
 
-        # experiment=self.experiment=self.logger.experiment
-        # if(epoch==0):
+        experiment=self.experiment=self.logger.experiment
+        if(epoch==0):
+            experiment.log_parameter('machine',os.environ['machine'])
