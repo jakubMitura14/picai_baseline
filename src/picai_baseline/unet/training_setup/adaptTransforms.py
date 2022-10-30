94b22fd960af55c07dc9c365a8532e2001ecfd51
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-30 15:17:27 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/training_setup/adaptTransforms.py
+++ src/picai_baseline/unet/training_setup/adaptTransforms.py
@@ -170,6 +170,7 @@ class applyOrigTransforms(MapTransform): #RandomizableTransform
             d[key] =  apply_transform(self.transform, d[key], map_items=False)
         return d
 def loadTrainTransform(transform,seg_transform,batchTransforms,normalizationIndex,normalizerDict,expectedShape):
+    print(f"hhhh {expectedShape}")
     return Compose([
             # printTransform(keys=["seg"],info=f"loadAndtransform "),
             loadImageMy(keys=["t2w","hbv","adc"],normalizationIndex=normalizationIndex,normalizerDict=normalizerDict),
@@ -186,6 +187,8 @@ def loadTrainTransform(transform,seg_transform,batchTransforms,normalizationInde
             monai.transforms.ToTensord(keys=["data","seg"], dtype=torch.float) 
              ]           )        
 def loadValTransform(transform,seg_transform,normalizationIndex,normalizerDict,expectedShape):
+    print(f"hhhh {expectedShape}")
+
     return Compose([
             # printTransform(keys=["seg"],info="loadAndtransform"),
 
