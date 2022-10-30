4ad907a57609d63e7e02b694906880c158288a58
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-30 15:05:06 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/LightningModel.py
+++ src/picai_baseline/unet/LightningModel.py
@@ -65,7 +65,7 @@ def getSegResNetb(dropout,input_image_size,in_channels,out_channels):
         in_channels=in_channels,
         out_channels=out_channels,
         dropout_prob=dropout,
-        blocks_down=(1, 2, 2, 4), blocks_up=(1, 1, 1)
+        # blocks_down=(1, 2, 2, 4), blocks_up=(1, 1, 1)
         #blocks_down=(2, 4, 4, 8), blocks_up=(2, 2, 2)
     )
 
