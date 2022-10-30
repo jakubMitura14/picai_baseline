9c3f7dbbd88a9e61ac635a95c3a98ac02e9354f7
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-30 19:20:19 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/LightningModel.py
+++ src/picai_baseline/unet/LightningModel.py
@@ -82,7 +82,7 @@ def getVNet(dropout,input_image_size,in_channels,out_channels):
         in_channels=4,
         out_channels=out_channels,
         dropout_prob=dropout
-    ),(4,32,256,256),8)
+    ),(4,32,256,256),6)
 
 def chooseModel(args,devicee,index, dropout, input_image_size,in_channels,out_channels  ):
     models=[#getSwinUNETRa(dropout,input_image_size,in_channels,out_channels),
