f6e4d61307263dc45dfa2b0d11b7020fabd2510f
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-31 11:18:53 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/LightningModel.py
+++ src/picai_baseline/unet/LightningModel.py
@@ -256,8 +256,8 @@ class Model(pl.LightningModule):
 
     def _shared_eval_step(self, valid_data, batch_idx):
         # print(f"ssshhh {valid_data['data'].shape}  label {valid_data['seg'].shape}")
-        valid_images = valid_data['data'][0,:,:,:,:]
-        valid_labels = valid_data['seg'][0,:,:,:,:]                
+        valid_images = valid_data['data']#[0,:,:,:,:]
+        valid_labels = valid_data['seg']#[0,:,:,:,:]                
         valid_images = [valid_images, torch.flip(valid_images, [4]).to(self.device)]
         preds = [
             torch.sigmoid(self.model(x))[:, 1, ...].detach().cpu().numpy().astype(np.float32)
