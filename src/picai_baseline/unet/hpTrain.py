68587980edc00bb7cecc96ce5d77f51ca4069532
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-31 16:14:03 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/hpTrain.py
+++ src/picai_baseline/unet/hpTrain.py
@@ -66,7 +66,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 
 def mainTrain(project_name,args,trial: optuna.trial.Trial,imageShape) -> float:
     swa_lrs=trial.suggest_float("swa_lrs", 1e-5,0.5) #trial.suggest_float("swa_lrs", 1e-6, 1e-4)
-    base_lr_multi =trial.suggest_float("base_lr_multi", 0.01, 10.0)
+    base_lr_multi =1.0#trial.suggest_float("base_lr_multi", 0.01, 10.0)
     schedulerIndex=1#trial.suggest_int("scheduler_int", 0, 1)
     modelIndex=trial.suggest_int("modelIndex", 0, 4)
     normalizationIndex=0#trial.suggest_int("normalizationIndex", 0, 1)
