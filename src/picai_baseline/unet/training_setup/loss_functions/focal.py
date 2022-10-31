dfc3b678d1e09e554303ef1036469beb471bf3a1
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-31 10:54:19 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/training_setup/loss_functions/focal.py
+++ src/picai_baseline/unet/training_setup/loss_functions/focal.py
@@ -30,8 +30,8 @@ class FocalLoss(nn.Module):
     def forward(self, inputs, targets):
         inputs = torch.sigmoid(inputs)
         targets = F.one_hot(targets, num_classes=self.num_classes).float()
-        targets = torch.moveaxis(targets, (0, 1, 2, 3, 4), (0, 2, 3, 4, 1))
-        
+        #targets = torch.moveaxis(targets, (0, 1, 2, 3, 4), (0, 2, 3, 4, 1))
+        print(f"in forward inputs{inputs.shape}  targets {targets.shape}  ")
         ce_loss = F.binary_cross_entropy(inputs, targets, reduction="none")
         p_t = (inputs[-1] * targets[-1]) + ((1 - inputs[-1]) * (1 - targets[-1]))
         loss = ce_loss * ((1 - p_t) ** self.gamma)
