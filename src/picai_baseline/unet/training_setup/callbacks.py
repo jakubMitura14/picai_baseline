3494f6e71b79d5458f896e7e2ef36b386417d9b3
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-31 18:14:17 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/training_setup/callbacks.py
+++ src/picai_baseline/unet/training_setup/callbacks.py
@@ -96,6 +96,62 @@ def resume_or_restart_training(model, optimizer, device, args, fold_id):
     return model, optimizer, tracking_metrics
 
 
+def resume_or_restart_training_tracking(args, fold_id):
+    """Resume/restart training, based on whether checkpoint exists"""
+
+    metrics_file = args.weights_dir + args.model_type + '_F' + str(fold_id) + "_metrics.xlsx"
+
+    if os.path.isfile(metrics_file):
+        try :
+            saved_metrics = pd.read_excel(metrics_file, engine='openpyxl')
+            all_epochs = (saved_metrics['epoch'].values).tolist()
+            all_valid_metrics_auroc = (saved_metrics['valid_auroc'].values).tolist()
+            all_valid_metrics_ap = (saved_metrics['valid_ap'].values).tolist()
+            all_valid_metrics_ranking = (saved_metrics['valid_ranking'].values).tolist()
+
+            tracking_metrics = {
+                'fold_id':                   fold_id,
+                'start_epoch':               -1,  # resume at next epoch
+                'all_epochs':                all_epochs,
+                'all_train_loss':           (saved_metrics['train_loss'].values).tolist(),
+                'all_valid_metrics_auroc':   all_valid_metrics_auroc,
+                'all_valid_metrics_ap':      all_valid_metrics_ap,
+                'all_valid_metrics_ranking': all_valid_metrics_ranking,
+                'best_metric':               np.max(all_valid_metrics_ranking),
+                'best_metric_epoch':         all_epochs[all_valid_metrics_ranking.index(
+                    np.max(all_valid_metrics_ranking))]}
+
+            print('Previous Record of Metrics Loaded:', metrics_file)
+        except:
+            tracking_metrics = {
+            'fold_id':                    fold_id,
+            'start_epoch':                -1,
+            'all_epochs':                 [],
+            'all_train_loss':             [],
+            'all_valid_metrics_auroc':    [],
+            'all_valid_metrics_ap':       [],
+            'all_valid_metrics_ranking':  [],
+            'best_metric': -1,
+            'best_metric_epoch': -1}
+    else:
+        print('Previous Record of Metrics Not Found:', metrics_file)
+
+        tracking_metrics = {
+            'fold_id':                    fold_id,
+            'start_epoch':                -1,
+            'all_epochs':                 [],
+            'all_train_loss':             [],
+            'all_valid_metrics_auroc':    [],
+            'all_valid_metrics_ap':       [],
+            'all_valid_metrics_ranking':  [],
+            'best_metric': -1,
+            'best_metric_epoch': -1}
+
+
+    return tracking_metrics
+
+
+
 def optimize_model(model, optimizer, loss_func, train_gen, args, tracking_metrics, device,writer):
     """Optimize model x N training steps per epoch + update learning rate"""
     
