ad86fff8610a693477b58309bb9bf787ee3aca14
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-31 12:30:46 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/LightningModel.py
+++ src/picai_baseline/unet/LightningModel.py
@@ -113,9 +113,9 @@ def save_heatmap(arr,dir,name,cmapp='gray'):
     return path
 
 def log_images(i,experiment,golds,extracteds ,labelNames, directory,epoch):
-    gold_arr_loc=golds[i]
+    gold_arr_loc=golds[i][0,:,:,:]
     extracted=extracteds[i][0,:,:,:]
-    labelName=labelNames[i][0,:,:,:]
+    labelName=labelNames[i]
     print(f"gggg gold_arr_loc {gold_arr_loc.shape} {type(gold_arr_loc)} extracted {extracted.shape} {type(extracted)}")
     maxSlice = max(list(range(0,gold_arr_loc.shape[2])),key=lambda ind : np.sum(gold_arr_loc[:,:,ind]) )
 
