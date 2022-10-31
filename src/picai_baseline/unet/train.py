c28630d9554b0e550da06a459c3c790d33b70fbd
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-31 16:15:03 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/train.py
+++ src/picai_baseline/unet/train.py
@@ -229,7 +229,7 @@ def main():
 
     args = parser.parse_args()
     
-    project_name= "pic_mono_4"
+    project_name= "pic_mono_5"
     #experiment_name="baseline_pl"
     # args.batch_size=32
     # imageShape=(256, 256,20)
