04313431bef72bbd4798b96f0103e86f5e8568bb
Jakub Mitura <jakub.mitura14@gmail.com>
2022-11-01 13:07:02 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/hpTrain.py
+++ src/picai_baseline/unet/hpTrain.py
@@ -118,7 +118,9 @@ def mainTrain(project_name,args,trial: optuna.trial.Trial,imageShape) -> float:
     #     schedulerIndex=1
     #     callbacks=[early_stopping,checkpoint_callback]
     logImageDir=tempfile.mkdtemp()
-    model = LightningModel.Model(f,args,args.base_lr,base_lr_multi,schedulerIndex,normalizationIndex,modelIndex,imageShape,fInd,logImageDir,dropout)
+    model = LightningModel.Model(f,args,args.base_lr,base_lr_multi
+            ,schedulerIndex,normalizationIndex,modelIndex,imageShape
+            ,fInd,logImageDir,dropout,regression_channels)
     #model = LightningModel.Model(f,args)
     trainer = pl.Trainer(
         #accelerator="cpu", #TODO(remove)
