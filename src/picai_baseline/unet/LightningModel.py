66e88b893187df47913dcc0ae2403f9341fece14
Jakub Mitura <jakub.mitura14@gmail.com>
2022-11-01 17:04:02 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/LightningModel.py
+++ src/picai_baseline/unet/LightningModel.py
@@ -290,11 +290,12 @@ class Model(pl.LightningModule):
 
     def _shared_eval_step(self, valid_data, batch_idx,dataloader_idx):
         valid_images = valid_data['data'][:,0,:,:,:,:]
-        valid_labels = valid_data['seg'][:,0,:,:,:,:]                
+        valid_labels = valid_data['seg'][:,0,:,:,:,:]
+        segmMap,reg_hat = self.modelRegression(valid_images)                
         valid_images = [valid_images, torch.flip(valid_images, [4]).to(self.device)]
         isCa = valid_data['isCa']
         label_name = valid_data['seg_name']
-        segmMap,reg_hat = self.modelRegression(valid_images)
+        
         if(dataloader_idx==0):
             self.regressionMetric_val(torch.round(reg_hat.flatten().float()),torch.Tensor(isCa).to(self.device).float())
         if(dataloader_idx==1):
