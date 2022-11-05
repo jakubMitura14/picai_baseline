79212e9e3f41668c6815255861f3401111cfe68d
Jakub Mitura <jakub.mitura14@gmail.com>
2022-11-05 06:38:11 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/hpTrain.py
+++ src/picai_baseline/unet/hpTrain.py
@@ -101,7 +101,6 @@ def mainTrain(project_name,args,trial: optuna.trial.Trial,imageShape) -> float:
     
 
 
-
     # stochasticAveraging=pl.callbacks.stochastic_weight_avg.StochasticWeightAveraging(swa_lrs= swa_lrs )
     early_stopping = pl.callbacks.early_stopping.EarlyStopping(
         monitor=toMonitor,
