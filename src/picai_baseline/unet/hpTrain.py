f4197be842f41354f6387101fa5af91ee4b33af2
Jakub Mitura <jakub.mitura14@gmail.com>
2022-11-13 08:06:21 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/hpTrain.py
+++ src/picai_baseline/unet/hpTrain.py
@@ -87,92 +87,91 @@ def mainTrain(project_name,args,trial: optuna.trial.Trial,imageShape) -> float:
 
     toMonitor="valid_ranking"
         # optuna_prune=PyTorchLightningPruningCallback(trial, monitor=toMonitor)     
-    # callbacks=[early_stopping], #optuna_prune
     # if(swa_lrs>0.0):
     #     callbacks=[early_stopping,stochasticAveraging], #optuna_prune
     # check_eval_every_epoch=40
-    check_eval_every_epoch=10
-
+    check_eval_every_epoch=5
     # for each fold
     fInd=-1
 
     for f in args.folds :#range(0, len(args.folds)):#args.folds:
         fInd=fInd+1
-        comet_logger = CometLogger(
-            api_key="yB0irIjdk9t7gbpTlSUPnXBd4",
-            #workspace="OPI", # Optional
-            project_name=project_name, # Optional
-            experiment_name=f"{machine}_{fInd}_{str(expId)}" # Optional
-            #experiment_name=experiment_name # Optional
-        )
-        # stochasticAveraging=pl.callbacks.stochastic_weight_avg.StochasticWeightAveraging(swa_lrs= swa_lrs )
-        early_stopping = pl.callbacks.early_stopping.EarlyStopping(
-            monitor=toMonitor,
-            patience=9,
-            mode="max",
-            #divergence_threshold=(-0.1)
-        )
-        # f=args.folds[fInd]
-        checkPointPath=f"/home/sliceruser/locTemp/checkPoints/{project_name}/{expId}/{fInd}"
-        checkpoint_callback = ModelCheckpoint(dirpath= checkPointPath,mode='max', save_top_k=1, monitor=toMonitor)
-        schedulerIndexToLog= schedulerIndex
-        callbacks=[early_stopping,checkpoint_callback]#stochasticAveraging
+        if(fInd>0):
+            comet_logger = CometLogger(
+                api_key="yB0irIjdk9t7gbpTlSUPnXBd4",
+                #workspace="OPI", # Optional
+                project_name=project_name, # Optional
+                experiment_name=f"{machine}_{fInd}_{str(expId)}" # Optional
+                #experiment_name=experiment_name # Optional
+            )
+            # stochasticAveraging=pl.callbacks.stochastic_weight_avg.StochasticWeightAveraging(swa_lrs= swa_lrs )
+            early_stopping = pl.callbacks.early_stopping.EarlyStopping(
+                monitor=toMonitor,
+                patience=7,
+                mode="max",
+                #divergence_threshold=(-0.1)
+            )
+            # f=args.folds[fInd]
+            checkPointPath=f"/home/sliceruser/locTemp/checkPoints/{project_name}/{expId}/{fInd}"
+            checkpoint_callback = ModelCheckpoint(dirpath= checkPointPath,mode='max', save_top_k=1, monitor=toMonitor)
+            schedulerIndexToLog= schedulerIndex
+            callbacks=[early_stopping,checkpoint_callback]#stochasticAveraging
 
-        logImageDir=tempfile.mkdtemp()
-        model = LightningModel.Model(f,args,args.base_lr,base_lr_multi
-                ,schedulerIndex,normalizationIndex,modelIndex,imageShape
-                ,fInd,logImageDir,dropout,regression_channels
-                ,RicianNoiseTransformProb
-                ,LocalSmoothingTransformProb
-                ,RandomBiasField_prob
-                ,RandomAnisotropy_prob
-                ,Random_GaussNoiseProb
-                ,optimizerIndex)
-        #model = LightningModel.Model(f,args)
-        trainer = pl.Trainer(
-            #accelerator="cpu", #TODO(remove)
-            max_epochs=3000,#args.num_epochs,
-            #gpus=1,
-            precision=16,#experiment.get_parameter("precision"), 
-            callbacks=callbacks, #optuna_prune
-            logger=comet_logger,
-            accelerator='auto',
-            devices='auto',       
-            default_root_dir= "/home/sliceruser/locTemp/lightning_logs",
-            # auto_scale_batch_size="binsearch",
-            auto_lr_find=True,
-            check_val_every_n_epoch=check_eval_every_epoch,
-            accumulate_grad_batches= 1,
-            #gradient_clip_val=  0.9 ,#experiment.get_parameter("gradient_clip_val"),# 0.5,2.0
-            log_every_n_steps=5
-            ,reload_dataloaders_every_n_epochs=1
-            #strategy='dp'
-        )
+            logImageDir=tempfile.mkdtemp()
+            model = LightningModel.Model(f,args,args.base_lr,base_lr_multi
+                    ,schedulerIndex,normalizationIndex,modelIndex,imageShape
+                    ,fInd,logImageDir,dropout,regression_channels
+                    ,RicianNoiseTransformProb
+                    ,LocalSmoothingTransformProb
+                    ,RandomBiasField_prob
+                    ,RandomAnisotropy_prob
+                    ,Random_GaussNoiseProb
+                    ,optimizerIndex)
+            #model = LightningModel.Model(f,args)
+            trainer = pl.Trainer(
+                #accelerator="cpu", #TODO(remove)
+                max_epochs=3000,#args.num_epochs,
+                #gpus=1,
+                precision=16,#experiment.get_parameter("precision"), 
+                callbacks=callbacks, #optuna_prune
+                logger=comet_logger,
+                accelerator='auto',
+                devices='auto',       
+                default_root_dir= "/home/sliceruser/locTemp/lightning_logs",
+                # auto_scale_batch_size="binsearch",
+                auto_lr_find=True,
+                check_val_every_n_epoch=check_eval_every_epoch,
+                accumulate_grad_batches= 1,
+                #gradient_clip_val=  0.9 ,#experiment.get_parameter("gradient_clip_val"),# 0.5,2.0
+                log_every_n_steps=5
+                ,reload_dataloaders_every_n_epochs=1
+                #strategy='dp'
+            )
 
-        trainer.tune(model)
+            trainer.tune(model)
 
-        experiment=trainer.logger.experiment
-        experiment.log_parameter('machine',machine)
-        experiment.log_parameter('base_lr_multi',base_lr_multi)
-        experiment.log_parameter('swa_lrs',swa_lrs)
-        experiment.log_parameter('schedulerIndex',schedulerIndexToLog)
-        experiment.log_parameter('normalizationIndex',normalizationIndex)
-        experiment.log_parameter('modelIndex',modelIndex)
+            experiment=trainer.logger.experiment
+            experiment.log_parameter('machine',machine)
+            experiment.log_parameter('base_lr_multi',base_lr_multi)
+            experiment.log_parameter('swa_lrs',swa_lrs)
+            experiment.log_parameter('schedulerIndex',schedulerIndexToLog)
+            experiment.log_parameter('normalizationIndex',normalizationIndex)
+            experiment.log_parameter('modelIndex',modelIndex)
 
-        experiment.log_parameter('dropout',dropout)
-        experiment.log_parameter('RicianNoiseTransformProb',RicianNoiseTransformProb)
-        experiment.log_parameter('LocalSmoothingTransformProb',LocalSmoothingTransformProb)
-        experiment.log_parameter('RandomBiasField_prob',RandomBiasField_prob)
-        experiment.log_parameter('RandomAnisotropy_prob',RandomAnisotropy_prob)
-        experiment.log_parameter('Random_GaussNoiseProb',Random_GaussNoiseProb)
-        experiment.log_parameter('optimizerIndex',optimizerIndex)
-        experiment.log_parameter('fInd',fInd)
-
-
-
+            experiment.log_parameter('dropout',dropout)
+            experiment.log_parameter('RicianNoiseTransformProb',RicianNoiseTransformProb)
+            experiment.log_parameter('LocalSmoothingTransformProb',LocalSmoothingTransformProb)
+            experiment.log_parameter('RandomBiasField_prob',RandomBiasField_prob)
+            experiment.log_parameter('RandomAnisotropy_prob',RandomAnisotropy_prob)
+            experiment.log_parameter('Random_GaussNoiseProb',Random_GaussNoiseProb)
+            experiment.log_parameter('optimizerIndex',optimizerIndex)
+            experiment.log_parameter('fInd',fInd)
 
 
-        trainer.fit(model)
+
+
+
+            trainer.fit(model)
 
 
 
