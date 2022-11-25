82296a08af8c290767375736705ef92f57a8bb3a
Jakub Mitura <jakub.mitura14@gmail.com>
2022-11-25 14:34:32 +0100

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- src/picai_baseline/unet/LightningModel.py
+++ src/picai_baseline/unet/LightningModel.py
@@ -248,13 +248,14 @@ class Model(pl.LightningModule):
         valid_images = valid_data['data']#[:,0,:,:,:,:].to(torch.float32)
         valid_labels = valid_data['seg']#[:,0,:,:,:,:].to(torch.float32)
         #segmMap = self.model(valid_images)
+        isCa = valid_data['isCa']
+        label_name = valid_data['seg_name']
 
         self.saveRegLoss(valid_images,dataloader_idx,isCa)
 
         valid_images = [valid_images, torch.flip(valid_images, [4]).to(self.device)]
         
-        isCa = valid_data['isCa']
-        label_name = valid_data['seg_name']
+
 
         
         preds = [
